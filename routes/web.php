2048c76e28df9246d9dcc86bc8fd4cb3ad945009
eritaputri <eritaputri0@gmail.com>
2022-04-25 15:11:17 +0700

    form register requester

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get("/register", function () {
-    return view('register.register', [
-        'title' => 'Register'
-    ]);
-});
+
 Route::get('/', function () {
     return view('welcome', [
         'title' => 'Requester'
