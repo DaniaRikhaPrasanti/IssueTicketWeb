4b89c72aa14847dacdbceb4ca9d35fbc4aba64b8
eritaputri <eritaputri0@gmail.com>
2022-05-18 20:53:53 +0700

    membuat controller migration dan model agent

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -2,89 +2,84 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Agent;
-use Illuminate\Support\Facedes\Storage;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class AgentController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $agent = DB::table('agent')->get();
-        // $nama = Auth::user()->name;
-
-
-        $data = array(
-            'menu' => 'agent',
-            'agent' => $agent,
-            'title' => 'Agent/List',
-            'submenu' => '',
-        );
-
-        return view('/agent/list',$data); 
+        //
     }
 
-    public function add_agent()
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        // $nama = Auth::user()->name;
-        $agent = DB::table('agent')->get();
-
-        $data = array(
-            'menu' => 'agent',
-            'agent' => $agent,
-            'title' => 'Agent/Tambah Agent',
-            'submenu' => '',
-        );
-
-        return view('/agent/add',$data); 
+        //
     }
 
-    public function insert_agent(Request $post)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-
-        DB::table('agent')->insert([    
-            'Ag_ID' => $post->Ag_ID,     
-            'Ag_Name' => $post->Ag_Name,     
-            'Ag_Email' => $post->Ag_Email,     
-            'Ag_No' => $post->Ag_No,     
-            'Ag_Address' => $post->Ag_Address,     
-            'Team_Status' => $post->Team_Status,     
-        ]);
-
-        // $agent = Agent::create(request(['Ag_ID','Ag_Name', 'Ag_Email', 'Ag_No', 'Ag_Address','Team_Status']));
-
-        return redirect('/agent');
+        //
     }
 
-    public function edit_agent($Ag_ID)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Agent  $agent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Agent $agent)
     {
-        // $nama = Auth::user()->name;
-        $agent = DB::table('agent')->where('Ag_ID',$Ag_ID)->get();
-
-        $data = array(
-            'menu' => 'agent',
-            'agent' => $agent,
-            'title' => 'Agent/Edit Agent',
-            'submenu' => '',
-        );
-
-        return view('/agent/edit',$data); 
+        //
     }
-    
 
-    public function update_agent(Request $post)
-    {   
-        DB::table('agent')->where('Ag_ID', $post->Ag_ID)->update([
-            'Ag_ID' => $post->Ag_ID,     
-            'Ag_Name' => $post->Ag_Name,     
-            'Ag_Email' => $post->Ag_Email,     
-            'Ag_No' => $post->Ag_No,     
-            'Ag_Address' => $post->Ag_Address,     
-            'Team_Status' => $post->Team_Status,    
-            ]);
-    
-            return redirect('/agent');
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Agent  $agent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Agent $agent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Agent  $agent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Agent $agent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Agent  $agent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Agent $agent)
+    {
+        //
     }
 }
