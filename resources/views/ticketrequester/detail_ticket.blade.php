246cd4a10d2471ee6c94937bac4b0210e2bb9225
juwitath <juwitatheomas@gmail.com>
2022-06-16 20:07:40 +0800

    optimize ticket controller show and view

--- resources/views/ticketrequester/detail_ticket.blade.php
+++ resources/views/ticketrequester/detail_ticket.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.appuser')
 @section("css")
 <style>
-  .timeline{list-style:none;padding:0 0 20px;position:relative;margin-top:-15px;border:1px solid #ccc; max-height:200px; overflow: scroll}
+  .timeline{list-style:none;padding:0 0 20px;position:relative;margin-top:-15px;border:1px solid #ccc; max-height:200px; overflow-y:auto}
   .timeline:before{top:30px;bottom:25px;position:absolute;content:" ";width:3px;background-color:#ccc;left:25px;margin-right:-1.5px}.timeline>li,.timeline>li>.timeline-panel{margin-bottom:5px;position:relative}.timeline>li:after,.timeline>li:before{content:" ";display:table}.timeline>li:after{clear:both}.timeline>li>.timeline-panel{margin-left:96px;float:left;top:19px;padding:4px 10px 8px 15px;border-radius:5px;width:45%}.timeline>li>.timeline-badge{color:#fff;width:72px;height:36px;font-size:1.2em;text-align:center;position:absolute;top:26px;left:9px;margin-right:-25px;background-color:#fff;z-index:100;border-radius:5px;border:1px solid #d4d4d4}.timeline>li.timeline-inverted>.timeline-panel{float:left}
   .timeline>li.timeline-inverted>
   .timeline-panel:before{border-right-width:0;border-left-width:15px;right:-15px;left:auto}
