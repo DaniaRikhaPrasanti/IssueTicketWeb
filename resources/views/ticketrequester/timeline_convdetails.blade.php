9ac3c59682409b9cfebb7465005286e10e160247
DaniaRikhaPrasanti <daniarikha8@gmail.com>
2022-07-11 11:44:34 +0700

    menambah tampilan detail ticket conv

--- resources/views/ticketrequester/timeline_convdetails.blade.php
+++ resources/views/ticketrequester/timeline_convdetails.blade.php
@@ -12,14 +12,28 @@
             <hr>
             <div class="form">
                 <form action="POST">
-                    <label for="Log_Creator" class="form-label">Pengirim : </label>
+                    <label for="Log_Creator" class="form-label">Requester : </label>
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1">
                             <i class="fas fa-user"></i>
                         </span>
                         <input type="text" class="form-control" id="Log_Creator" name="Log_Creator" required value="{{ $ticketConv->Log_Creator }}" ria-label="Username" aria-describedby="basic-addon1"disabled>
                     </div>
-                        
+                    <label for="Log_Title" class="form-label">Title : </label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">
+                            <i class="fas fa-user"></i>
+                        </span>
+                        <input type="text" class="form-control" id="Log_Title" name="Log_Title" required value="{{ $ticketConv->Log_Title }}" ria-label="Username" aria-describedby="basic-addon1"disabled>
+                    </div>
+                    <label for="Log_Desc" class="form-label">Desc : </label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">
+                            <i class="fas fa-user"></i>
+                        </span>
+                        <input type="text" class="form-control" id="Log_Desc" name="Log_Desc" required value="{{ $ticketConv->Log_Desc }}" ria-label="Username" aria-describedby="basic-addon1"disabled>
+                    </div>
+                    
                     
 
                     <!-- <div class="text-right mt-4">
